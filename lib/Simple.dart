82000afa602ba949c586edadb8542c7212f959ac
zoyasarfraz <zoyas26870@gmqail.com>
2020-12-05 15:54:54 +0500

    simple

---
+++ lib/Simple.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:rflutter_alert/rflutter_alert.dart';
+class Simple extends StatefulWidget {
+  @override
+  _State createState() => _State();
+
+  class Simple extends StatelessWidget {
+  }
+  class _State extends State<Simple> {
+  int LeftButton = 1;
+  int RightButton = 1;
+  int countleft=0;
+  int countright=0;
+  @override
+  Widget build(BuildContext context) {
+  return MaterialApp(
+  home: Scaffold(
+  backgroundColor: Colors.grey.shade900,
+  body: SafeArea(
+  child: Text(
+  'Simple',
+  style: TextStyle(
+  color: Colors.white,
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+  return  MaterialApp(
+  home: Scaffold(
+  backgroundColor: Colors.red,
+  appBar: AppBar(
+  title: Text('Simple'),
+  backgroundColor: Colors.red,
+  ),
+  body: Center(
+  child: Column(
+  children: <Widget>[
+  Row(
+  children: <Widget>[
+  Expanded(
+  //flex: 2,
+  child:  RaisedButton(
+  onPressed: (){
+  if(countleft>countright){
+  Alert(
+  context: context,
+  title: 'Winer',
+  desc:
+  'Left Dice is Winer',
+  ).show();
+  }
+  else if(countright>countleft){
+  Alert(
+  context: context,
+  title: 'Winer',
+  desc:
+  'Right Dice is Winer',
+  ).show();
+  }
+  else{
+  Alert(
+  context: context,
+  title: 'Winer',
+  desc:
+  'Both Dice are Same',
+  ).show();
+  }
+  countleft=0;
+  countright=0;
+
+  },
+  child: Text(
+  'Winer',
+  style: TextStyle(
+  color: Colors.white,
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+  ),
+  ),
+  )
+  ),
+  ],
+  ),
+  ),
+  Row(
+  children: <Widget>[
+  Expanded(
+  //flex: 2,
+  child: FlatButton(
+  onPressed: () {
+  setState(() {
+  RandNum();
+  });
+  },
+  child: Image.asset('images/dice$LeftButton.png')
+  ),
+  ),
+  Expanded(
+  //flex: 1,
+  child: FlatButton(
+  onPressed: () {
+  setState(() {
+  RandNum();
+  });
+  },
+  child: Image.asset('images/dice$RightButton.png')
+  ),
+  ),
+  ],
+  ),
+  ],
+  ),
+  ));
+
+
+  ),
+  ),
+  );
+  }
+  void RandNum(){
+  LeftButton = Random().nextInt(6) + 1;
+  RightButton = Random().nextInt(6) + 1;
+  countleft=countleft+LeftButton;
+  countright=countright+RightButton;
+  }
+  }
--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,58 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:splashscreen/splashscreen.dart';
-void main(){
-  runApp(new MaterialApp(
-    home: new MyApp(),
-  ));
+void main() {
+  runApp(MyApp());
 }
 
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new SplashScreen(
-        seconds: 60,
-        navigateAfterSeconds: new AfterSplash(),
-        title: new Text('Zoya',
-          style: new TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20.0
-          ),),
-        image: new Image.network(' https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F2218352%2Fpexels-photo-2218352.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D1%26w%3D500&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fhijab%2F&tbnid=wApfPdhbEBBWfM&vet=12ahUKEwi9gaWMtrbtAhVJ44UKHTeGA_kQMygHegUIARDkAQ..i&docid=yGGFlgLFI1xtbM&w=500&h=331&q=girls%20hijab%20pic&ved=2ahUKEwi9gaWMtrbtAhVJ44UKHTeGA_kQMygHegUIARDkAQ'),
-        backgroundColor: Colors.white,
-        styleTextUnderTheLoader: new TextStyle(),
-        photoSize: 100.0,
-        onClick: ()=>print("Flutter Egypt"),
-        loaderColor: Colors.blue
+    return MaterialApp(
+      title: 'Zoya',
+
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+
+
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class AfterSplash extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 30),
+            ()=>Navigator.pushReplacement(context,
+            MaterialPageRoute(builder:
+                (context) =>
+                SecondScreen()
+            )
+        )
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-          title: new Text("Welcome In SplashScreen Package"),
-          automaticallyImplyLeading: false
-      ),
-      body: new Center(
-        child: new Text("Done!",
-          style: new TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30.0
-          ),),
+    return Container(
+        color: Colors.white,
+        child:FlutterLogo(size:MediaQuery.of(context).size.height)
+    );
+  }
+}
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text("Home Page")),
+      body: Center(
+          child:Text("Done",textScaleFactor: 2,)
 
       ),
     );
